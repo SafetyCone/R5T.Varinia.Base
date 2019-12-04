9f7610a448e536f9a987e96423b06c544904b541
MinexAutomation <minex.automation@gmail.com>
2019-12-04 10:47:17 -0800

    Added location repository interface.

--- source/R5T.Varinia.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Varinia.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Varinia.Base/Code/Services/Interfaces/ILocationRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Varinia
+{
+    public interface ILocationRepository
+    {
+        LocationIdentity New();
+        LocationIdentity New(LngLat lngLat);
+
+        bool Exists(LocationIdentity identity);
+
+        void SetLngLat(LocationIdentity identity, LngLat lngLat);
+        LngLat GetLngLat(LocationIdentity identity);
+
+        void Delete(LocationIdentity identity);
+    }
+}
